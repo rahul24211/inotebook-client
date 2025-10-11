f0693a91f8554c42a77841297ea724ac091da8b4
Rahul Kumar <rahulpro886@gmail.com>
2025-10-11 10:53:38 +0530

    complete

--- src/pages/notes/logStateContext.js
+++ src/pages/notes/logStateContext.js
@@ -10,6 +10,7 @@ const LogStateContext = (props) => {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
+                
             },
             body: JSON.stringify(formData)
         })
